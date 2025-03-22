fed9e562d7905ef2728f9f6206d3d4bd5c75fd24
hugocesarcoelho <hugo_cesar45@hotmail.com>
2025-03-22 13:10:40 -0300

    test

--- main.tf
+++ main.tf
@@ -194,7 +194,7 @@ resource "aws_lambda_permission" "api_gateway" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "api" {
   depends_on  = [aws_api_gateway_integration.integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "prod"
